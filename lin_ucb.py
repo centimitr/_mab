1e797c4117c1ca2814f941dafd0ec52a80fe06f3
cmp <devbycm@gmail.com>
2018-10-02 23:18:19 +1000

    update

--- lin_ucb.py
+++ lin_ucb.py
@@ -1,7 +1,6 @@
 from mab import MAB
 import numpy as np
 from numpy.linalg import inv
-import matplotlib.pyplot as plt
 
 
 class LinUCB(MAB):
@@ -24,24 +23,24 @@ class LinUCB(MAB):
         self.narms = narms
         self.ndims = ndims
         self.alpha = alpha
-        self.Aa_dict = {arm: np.identity(ndims) for arm in range(1, narms)}
-        self.ba_dict = {arm: np.zeros(ndims) for arm in range(1, narms)}
+        self.arm_range = range(1, narms + 1)
+        self.Aa_dict = {arm: np.identity(ndims) for arm in self.arm_range}
+        self.ba_dict = {arm: np.zeros(ndims) for arm in self.arm_range}
         return
 
     def _ctx_for_arm(self, context, arm):
         return context[self.ndims * (arm - 1):self.ndims * arm]
 
-    def play(self, tround, context):
-        def p_fn(alpha, Aa, ba, Xa):
-            Aa_inv = inv(Aa)
-            theta = np.dot(Aa_inv, ba)
-            p = np.dot(theta.T, Xa) + alpha * np.sqrt(np.dot(np.dot(Xa.T, Aa_inv), Xa))
-            return p
+    def _p_fn(self, Aa, ba, Xa):
+        Aa_inv = inv(Aa)
+        theta = np.dot(Aa_inv, ba)
+        p = np.dot(theta.T, Xa) + self.alpha * np.sqrt(np.dot(np.dot(Xa.T, Aa_inv), Xa))
+        return p
 
+    def play(self, tround, context):
         # choose arm with max p value
-        p_values = [
-            p_fn(self.alpha, self.Aa_dict[arm], self.ba_dict[arm], self._ctx_for_arm(context, arm)) for arm in
-            range(1, self.narms)]
+        p_values = [self._p_fn(self.Aa_dict[arm], self.ba_dict[arm], self._ctx_for_arm(context, arm))
+                    for arm in self.arm_range]
         idx = np.argmax(p_values)
         arm = idx + 1
         return arm
